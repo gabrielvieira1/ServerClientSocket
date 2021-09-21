21980cb557b5685f1507ef66c27a37ca08e586b7
Gabriel Vieira <gabrielvieira1@protonmail.com>
2021-09-21 03:16:02 -0300

    Create project

---
+++ ServidorSocket/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static ServerClientSocket.AsynchronousSocketListener;
+using static ServerClientSocket.AsynchronousClient;
+
+namespace ServerClientSocket
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //StartClient();
+      StartListening();
+    }
+  }
+}
